2f52c8ed71ddc16cee033429c04c0851bb471a02
ms2dwrld <hammedomoniyi3@gmail.com>
2026-06-26 12:31:42 +0100

    added routing in the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./components/layout";
+
+import Home from "./pages/Home";
+import Signup from "./components/signup";
 import Login from "./components/login";
 
-function App() {
+const App = () => {
   return (
-    <Login/>
+    <BrowserRouter>
+      <Routes>
+        {/* Pages with Navbar + Footer */}
+        <Route
+          path="/"
+          element={
+              <Home />
+          }
+        />
+
+        {/* Auth Pages without Layout */}
+        <Route path="/signup" element={<Signup />} />
+
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
